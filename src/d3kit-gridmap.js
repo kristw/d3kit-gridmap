ae543f969ada5503f63d8f991f39b58e63724b4e
Robert Harris <rharris@twitter.com>
2016-03-07 17:59:30 -0800

    add duration function option

--- src/d3kit-gridmap.js
+++ src/d3kit-gridmap.js
@@ -6,6 +6,7 @@ var DEFAULT_OPTIONS = {
   initialWidth: 500,
   initialHeight: 340,
   keyFn: undefined,
+  durationFn: () => 250,
   row: function(d){return d.row;},
   col: function(d){return d.col;},
   text: function(d){return d.key;},
@@ -85,7 +86,7 @@ function constructor(skeleton){
       .style('text-anchor', options.textAnchor)
       .text(options.text);
 
-    var sTrans = selection.transition()
+    var sTrans = selection.transition().duration(options.durationFn)
       .attr('transform', function(d){
         return 'translate('+(options.col(d)*options.tileWidth)+','+(options.row(d)*options.tileHeight)+')';}
       );
